9586b197b9451f8896c8daef574441c079d9e812
Dinver18 <117117769+Dinver18@users.noreply.github.com>
2024-09-11 14:09:27 -0400

    Se creo el footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,27 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layouts/Navbar';
-import Header from './components/layouts/Header'; 
+import Header from './components/layouts/Header';
 import Services from './pages/OurServices/OurServices';
 import Contact from './pages/Contact/Contact';
 import AboutUs from './pages/AboutUs/AboutUs'; // Importa tu Navbar
+import Footer from './components/layouts/Footer';
 
 function App() {
   return (
     <>
-  <Router>
-      <Header />
-      <Navbar />
-      <div className="container mx-auto "> {/* Ajuste con margen superior */}
-        <Routes>
-          <Route path="/Ourservices" element={<Services />} />
-          <Route path="/Contact" element={<Contact />} />
-          <Route path="/AboutUs" element={<AboutUs />} />
-        </Routes>
-      </div>
-    </Router>
-
-      
-      
- 
+      <Router>
+        <Header />
+        <Navbar />
+        <div className="container mx-auto "> {/* Ajuste con margen superior */}
+          <Routes>
+            <Route path="/Ourservices" element={<Services />} />
+            <Route path="/Contact" element={<Contact />} />
+            <Route path="/AboutUs" element={<AboutUs />} />
+          </Routes>
+        </div>
+        <Footer/>
+      </Router>
     </>
   );
 }

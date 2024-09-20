b0db1168ca5db267feca9556cf47057e5c57077c
Dinver18 <117117769+Dinver18@users.noreply.github.com>
2024-09-19 21:51:41 -0400

    Se agrego el logo de la clinica

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,8 +1,10 @@
+import Logo from "../../assets/Logo.jpg"
+
 function Header() {
   return (
     <section className="justify-start bg-gray-100 p-2 ">
       <div className="flex items-center">
-        <img src="/vite.svg" alt="Logo" className="h-12 sm:h-20 mr-2 sm:mr-4" />
+        <img src={Logo} alt="Logo" className="h-12 sm:h-20 mr-2 sm:mr-4" />
         <div>
           <h1 className="text-xl sm:text-2xl font-bold text-gray-800">Clínica Popular El Paraíso</h1>
           <p className="text-sm sm:text-lg text-gray-600">Tu salud es nuestra prioridad</p>
